7c1b422aa3598841e599bcfe3da93075c810df7d
varjain <varjain@redhat.com>
2023-04-10 00:57:28 +0530

    beautify product cards

--- src/body/Product.js
+++ src/body/Product.js
@@ -3,8 +3,8 @@ function Product({data}) {
         <li className="product">
             <p>{data.Mobile}</p>
             <div className="price">
-                <p>MRP <del>{data.MRP}</del></p>
-                <p>Our Price {data.Price}</p>
+                <p>MRP &#8377;<del>{data.MRP}</del></p>
+                <p className="our-price">Our Price &#8377;{data.Price}</p>
             </div>
         </li>
      );
