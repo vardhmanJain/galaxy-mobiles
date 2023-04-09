3993bfe3143a74faec1b0200f1dfe0d6af366d59
varjain <varjain@redhat.com>
2023-04-09 20:49:41 +0530

    fix searching for any case

--- src/body/Search.js
+++ src/body/Search.js
@@ -5,7 +5,7 @@ function Search({datum, setDatum, persistentData}) {
 
     function handleChange(e){
         e.preventDefault();
-        datum = e.target.value === "" ? persistentData: persistentData.filter(data => data.Mobile.toLowerCase().includes(e.target.value));
+        datum = e.target.value === "" ? persistentData: persistentData.filter(data => data.Mobile.toLowerCase().includes(e.target.value.toLowerCase()));
         setDatum(datum);
         setText(e.target.value);
     }
